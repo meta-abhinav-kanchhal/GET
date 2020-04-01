3d689ad797ede5a282ef22ee6faa141c1dfe3d10
Abhinav Kanchhal <abhinav.kanchhal@metacube.com>
2020-04-01 20:56:36 +0530

    dsa 3 done

---
+++ DSAAssignment3/src/priorityQueue/Queue.java
@@ -0,0 +1,22 @@
+package priorityQueue;
+
+/*@author Abhinav Kanchhal
+ * interface of Queue
+ * */
+public class Queue {
+	int priority;
+	String process;
+	
+	Queue(String process,int priority){
+		this.priority = priority;
+		this.process = process;
+	}
+	
+	public int getPriority(){
+		return this.priority;
+	}
+	
+	public String getProcess(){
+		return this.getProcess();
+	}
+}
